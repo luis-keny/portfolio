447084580c6ca00d2f80c835f804410cb7706103
luis-keny <lucerobalvinluiskeny@gmail.com>
2025-03-03 13:59:30 -0500

    chore: contact link in header and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,8 @@ import { ExternalLink } from "lucide-react";
 
 export default function Footer() {
   return (
-    <footer className="py-2 dark:text-gray-400/80 flex gap-1 items-center pt-12">
+    <footer className="py-2 dark:text-gray-400/80 flex gap-10 items-center justify-between pt-12">
+      <div className="flex gap-1 items-center">
         <span>Inspired by</span>
         <Link 
           href="https://porfolio.dev/"
@@ -13,6 +14,10 @@ export default function Footer() {
           porfolio.dev
           <ExternalLink className="" size={16}/>
         </Link>
+      </div>
+      <div className="dark:text-gray-400/80">
+        <a id="contacto" href="mailto:miduga@gmail.com" className="hover:underline">Contact</a>
+      </div>
     </footer>
   )
 }
