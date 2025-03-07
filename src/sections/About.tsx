72e5862503bffeed19ae7eac7ed36ddc37774c2f
luis-keny <lucerobalvinluiskeny@gmail.com>
2025-03-07 10:34:00 -0500

    chore: english translation about me

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -4,24 +4,24 @@ export default function About() {
   return (
     <section id="about" className="pt-14">
       <div className="flex gap-2 items-center">
-        <UserCheck2 size={32}/>
+        <UserCheck2 size={32} />
         <h2 className="text-3xl font-semibold">About</h2>
       </div>
       <main className="mt-8 description space-y-3">
         <p>
-          Soy <span>Luis Keny</span>, apasionado por la <span>programación y el desarrollo de software. </span>
-          A lo largo de mi carrera, he tenido la oportunidad de <span>desarrollar múltiples aplicaciones web </span> 
-          , lo que me ha permitido profundizar en soluciones tecnológicas reales.
+          I am <span>Luis Keny</span>, passionate about <span>programming and software development.</span>
+          Throughout my career, I have had the opportunity to <span>develop multiple web applications</span>,
+          which has allowed me to dive into real technological solutions.
         </p>
         <p>
-          Actualmente, junto con un grupo de amigos, estamos <span>creando una empresa consultora dedicada 
-          al desarrollo de software</span>, con el objetivo de ofrecer <span>soluciones innovadoras</span> a 
-          diversos sectores. Me gusta programar porque me permite descomponer problemas complejos de manera más 
-          granular y encontrar soluciones efectivas.
+          Currently, together with a group of friends, we are <span>creating a consulting company dedicated
+            to software development</span>, with the goal of offering <span>innovative solutions</span> to various
+          sectors. I enjoy programming because it allows me to break down complex problems in a more granular way
+          and find effective solutions.
         </p>
         <p>
-          Además, disfruto de una buena taza de café mientras trabajo, lo que me ayuda a mantenerme enfocado 
-          y motivado. <span>Estoy entusiasmado por continuar mi camino profesional y aportar a la comunidad tecnológica.</span>
+          In addition, I enjoy a good cup of coffee while working, which helps me stay focused and motivated.
+          <span>I am excited to continue my professional journey and contribute to the tech community.</span>
         </p>
       </main>
     </section>
