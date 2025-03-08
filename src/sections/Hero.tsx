0fa76ae0f15959bbfff7633c06c4bb3075bdd1ef
luis-keny <lucerobalvinluiskeny@gmail.com>
2025-03-08 16:31:34 -0500

    chore: Adding contact button and hiding the github button when in movile in hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,21 +1,29 @@
 "use client";
  
 import { cn } from "@/lib/utils";
-import { Github, Linkedin } from "lucide-react";
+import { Github, Linkedin, Mail } from "lucide-react";
 import { GridPattern } from "@/components/magicui/grid-pattern";
 import Link from "next/link";
 import { BorderBeam } from "@/components/magicui/border-beam";
 
 const SOCIALS_MEDIA = [
   {
-    name: "github",
+    name: "Github",
     href: "https://github.com/luis-keny",
     icon: <Github/>,
+    target: "_blank",
+    movilHidden: true,
   },
   {
-    name: "linkedin",
+    name: "Linkedin",
     href: "https://www.linkedin.com/in/luis-keny-lucero-balvin-403a8520a/",
     icon: <Linkedin/>,
+    target: "_blank",
+  },
+  {
+    name: "Contact me",
+    href: "mailto:luis.lucero03@outlook.com",
+    icon: <Mail/>,
   },
 ];
 
@@ -51,14 +59,15 @@ export default function Hero() {
           </span>
         </div>
         <p className="text-gray-600 dark:text-gray-400 text-xl">{"Specialist in software development, creating modern and attractive websites that meet the user's needs."}</p>
-        <div className="flex gap-4">
+        <div className="flex gap-x-3 gap-y-2 flex-wrap">
           {
-            SOCIALS_MEDIA.map(({name, icon, href})=>(
+            SOCIALS_MEDIA.map(({name, icon, href, target, movilHidden})=>(
             <Link 
               key={`${name}-social-media`}
               href={href} 
-              target="_blank" 
-              className="flex gap-2 items-center bg-teal-800/20 border border-teal-400 w-fit px-4 py-2 rounded-xl hover:bg-teal-800/30 dark:hover:bg-teal-700/30 transition-all"
+              target={target} 
+              className={`flex gap-2 items-center bg-teal-800/20 border border-teal-400 w-fit px-4 py-2 
+                rounded-xl hover:bg-teal-800/30 dark:hover:bg-teal-700/30 transition-all ${movilHidden && "hidden sm:flex"}`}
             >
               {icon}
               <span>{name}</span>
